687d5c76a051a0712605aa54658f1cbd0be712f9
Jeferson <jhernandez0642@misena.edu.co>
2021-05-28 16:35:51 -0500

    Agregar archivos de proyecto.

---
+++ EmpresaImportadoraDC/Controllers/TransportadorasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpresaImportadoraDC.Controllers
+{
+    public class TransportadorasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
