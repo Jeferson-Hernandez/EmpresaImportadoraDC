2fa183b4e52bc1bdb68d12ac75a0fb5d23881aca
DanielP <danielpolo210@gmail.com>
2021-06-03 11:31:37 -0500

    Cliente

--- EmpresaImportadoraDC/Controllers/ClientesController.cs
+++ EmpresaImportadoraDC/Controllers/ClientesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmpresaImportadoraDC.Models.Abstract;
+using EmpresaImportadoraDC.Models.Entities;
+using EmpresaImportadoraDC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,47 @@ namespace EmpresaImportadoraDC.Controllers
 {
     public class ClientesController : Controller
     {
-        public IActionResult Index()
+        private readonly IClienteService _clienteService;
+        public ClientesController(IClienteService clienteService)
         {
-            return View();
+            _clienteService = clienteService;
         }
+        [HttpGet]
+        public async Task <IActionResult> Index()
+        {
+        return View(await _clienteService.ObtenerListaClientes());
+        }
+        [HttpGet]
+        public async Task<IActionResult> CrearEditarClientes(int id=0)
+        {
+            if (id == 0)
+                return View(new ClienteViewModel());
+            else
+            {              
+                Cliente cliente = await _clienteService.ObtenerClientePorId(id);
+                ClienteViewModel clienteViewModel = new()
+                {
+                    NombreCliente = cliente.NombreCliente,
+                    Correo = cliente.Correo,
+                    DireccionEntrega = cliente.DireccionEntrega
+                };
+                return View(clienteViewModel);
+            }
+        }
+         [HttpPost]     
+        public async Task<IActionResult> EliminarEmpleado(int id)
+        {
+            try
+            {
+                await _clienteService.EliminarCliente(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
     }
 }
--- EmpresaImportadoraDC/Models/Business/ClienteService.cs
+++ EmpresaImportadoraDC/Models/Business/ClienteService.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using EmpresaImportadoraDC.Models.Abstract;
+using EmpresaImportadoraDC.Models.DAL;
+using EmpresaImportadoraDC.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmpresaImportadoraDC.Models.Business
 {
-    public class ClienteService
+    public class ClienteService : IClienteService
     {
+        private readonly AppDbContext _context;
+
+        public ClienteService(AppDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IEnumerable<Cliente>> ObtenerListaClientes()
+        {
+            return await _context.Cliente.ToListAsync();
+        }
+        public async Task RegistrarCliente(Cliente cliente)
+        {
+            _context.Add(cliente);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Cliente> ObtenerClientePorId(int id)
+        {
+            //return await _context.Empleados.Include(x => x.Cargo).FindAsync(id);
+            return await _context.Cliente.FirstOrDefaultAsync(x => x.ClienteId == id);
+        }
+        public async Task EditarCliente(Cliente cliente)
+        {
+            _context.Update(cliente);
+            await _context.SaveChangesAsync();
+        }
+        public async Task EliminarCliente(int id)
+        {
+            var cliente = await ObtenerClientePorId(id);
+            _context.Remove(cliente);
+            await _context.SaveChangesAsync();
+        }
+
     }
 }
