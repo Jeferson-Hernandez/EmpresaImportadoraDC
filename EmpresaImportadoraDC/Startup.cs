d1cb1185bf88e88ec72694455c5ae81a644ec5e8
Daniel <daniel323agudelo@gmail.com>
2021-06-07 15:00:05 -0500

    Transportadora 2

--- EmpresaImportadoraDC/Startup.cs
+++ EmpresaImportadoraDC/Startup.cs
@@ -59,7 +59,7 @@ namespace EmpresaImportadoraDC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Paquetes}/{action=Index}/{id?}");
+                    pattern: "{controller=Transportadoras}/{action=Index}/{Id?}");
             });
         }
     }
